c43b42b634454a452e53da3241cc8bfcd8a79755
DANIEL-PARDO\Daniel Pardo <danielpardo@devanddel.com>
2022-03-25 11:31:25 +0100

    Diagrama corregido, plantilla de archivos routes y error

---
+++ src/features/categories/utils/category.error.ts
@@ -0,0 +1,14 @@
+export const CATEGORY_ERRORS = {
+    notExists: "Category not exists",
+    alreadyExists: "Category already exists",
+
+    notFound: "Category not found",
+    notProvided: "No category provided",
+
+    name: {
+        notProvided: "No category name provided",
+    },
+    description: {
+        notProvided: "No category description provided",
+    }
+};
