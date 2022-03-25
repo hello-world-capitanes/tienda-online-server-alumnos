7a496b9692fb881e3c3852c3b75aab002d2423cd
nolo98 <manuelm_98@hotmail.es>
2022-03-25 15:24:31 +0100

    Error solve conflict

--- src/features/categories/utils/category.error.ts
+++ src/features/categories/utils/category.error.ts
@@ -2,8 +2,6 @@ export const CATEGORY_ERRORS = {
     notExists: "Category not exists",
     alreadyExists: "Category already exists",
 
-    id:"Category id doesnt exit",
-
     notFound: "Category not found",
     notProvided: "No category provided",
 
