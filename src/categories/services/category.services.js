75b81c49732efd5e5cf939ca88182ec914731336
raulpradanas <raulpradanas@devanddel.com>
2022-03-25 11:22:19 +0100

    Merge para la estructura de carpetas

--- src/categories/services/category.services.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.promises.readFile("../data/category.json")
-.then(function(result) {
-  console.log(""+result);
-})
-.catch(function(error) {
-   console.log(error);
-})
